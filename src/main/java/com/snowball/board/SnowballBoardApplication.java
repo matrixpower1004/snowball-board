b05e8608f2dc9ee86bff46fcd9338cd17ab9417e
matrixpower1004 <matrixpower1004@gmail.com>
2023-07-10 15:00:18 +0900

    Initlalize: #1 - project setup
    * 프로젝트 시작을 위한 dependencies 설정
    * Java 17, JPA, MyBatis, H2 DB, Validation, Spring Web, Lombok
    * Spring Security는 설정이 완성될 때까지 잠시 주석 처리
    * 기본 프로퍼티 설정

---
+++ src/main/java/com/snowball/board/SnowballBoardApplication.java
@@ -0,0 +1,13 @@
+package com.snowball.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowballBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowballBoardApplication.class, args);
+    }
+
+}
