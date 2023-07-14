c4ecd8540062e3d0887a7f52a2c86c27469f87f7
JY-B <jongyoonb+u@gmail.com>
2023-07-15 03:25:01 +0900

    feat: #17 - modify User model for JPA
    update User model
     - modify annotation for JPA
     - add constrains each fields

--- src/main/java/com/snowball/board/domain/user/model/User.java
+++ src/main/java/com/snowball/board/domain/user/model/User.java
@@ -1,29 +1,45 @@
 package com.snowball.board.domain.user.model;
 
-import lombok.*;
 import com.snowball.board.common.util.UserRole;
+import org.hibernate.annotations.DynamicInsert;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode
-@Builder
+@Entity
+@DynamicInsert
+@Table(name = "user_tb")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // 유저의 Unique ID (로그인 ID)
+
+    @Column(name = "user_account", length = 15, nullable = false, unique = true)
     private String userAccount;
+
+    @Column(name = "user_name", length = 10, nullable = false)
     private String userName;
+
+    @Column(length = 72, nullable = false)
     private String password;
+
+    @Column(length = 30, nullable = false)
     private String email;
+
+    @Column(name = "nick_name", length = 10, nullable = false, unique = true)
     private String nickName;
-    // TRUE(Activated), FALSE(UnActivated)
-    private Boolean userStatus;
-    // 회원의 등급
-    private UserRole userRole;
+
+    @Column(name = "user_status", nullable = false, columnDefinition = "boolean default true")
+    private boolean userStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role", length = 20, nullable = false, columnDefinition = "varchar(20) default 'BEGINNER'")
+    private UserRole userRole = UserRole.BEGINNER;
+
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "timestamp default current_timestamp")
     private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false, columnDefinition = "timestamp default current_timestamp")
     private Timestamp updatedAt;
 }
