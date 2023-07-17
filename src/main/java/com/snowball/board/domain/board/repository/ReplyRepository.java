8044989f9a88bcac9af3c1ceee07e93e10c87d7c
ParkSeongUk <tjddnr633@naver.com>
2023-07-17 13:06:21 +0900

    대댓글 기능 구현

---
+++ src/main/java/com/snowball/board/domain/board/repository/ReplyRepository.java
@@ -0,0 +1,12 @@
+package com.snowball.board.domain.board.repository;
+
+import com.snowball.board.domain.board.emtity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findAllByCommentId(Long commentId);
+}
