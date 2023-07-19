b9c5601018a47ae087e14534e2a5c986d3cc8467
JY-B <jongyoonb+u@gmail.com>
2023-07-19 12:55:21 +0900

    feat: #11 - modify AuthorizationFilter
    rename JwtFilter to AuthorizationFilter
    add methods to save, valid check, revoke token

--- src/main/java/com/snowball/board/config/AuthorizationFilter.java
+++ src/main/java/com/snowball/board/config/AuthorizationFilter.java
@@ -1,7 +1,11 @@
 package com.snowball.board.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.snowball.board.common.exception.dto.ExceptionDto;
+import com.snowball.board.common.exception.message.AuthExceptionMessage;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,9 +20,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class AuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
@@ -27,19 +35,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userAccount;
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (request.getServletPath().contains("/api/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
+        final String authHeader = request.getHeader("Authorization");
+        final String accessToken;
+        final String userAccount;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            response.setStatus(SC_BAD_REQUEST);
+            response.setContentType(APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("utf-8");
+            ExceptionDto errorResponse = ExceptionDto.builder()
+                    .message(AuthExceptionMessage.TOKEN_NOT_FOUND.message())
+                    .statusCode(HttpStatus.BAD_REQUEST.value())
+                    .build();
+            new ObjectMapper().writeValue(response.getWriter(), errorResponse);
+        }
         //"Bearer " -> 6 length
-        jwt = authHeader.substring(7);
-        userAccount = jwtService.extractUserAccount(jwt);
+        accessToken = authHeader.substring(7);
+        userAccount = jwtService.extractUserAccount(accessToken);
         if (userAccount != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userAccount);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            if (jwtService.isTokenValid(accessToken, userDetails)) {
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
@@ -49,8 +67,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                filterChain.doFilter(request, response);
+            } else {
+                response.setStatus(SC_UNAUTHORIZED);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                response.setCharacterEncoding("utf-8");
+                ExceptionDto errorResponse = ExceptionDto.builder()
+                        .message(AuthExceptionMessage.FAIL_TOKEN_CHECK.message())
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .build();
+                new ObjectMapper().writeValue(response.getWriter(), errorResponse);
             }
+
         }
-        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/snowball/board/domain/auth/dto/AuthenticationResponse.java
+++ src/main/java/com/snowball/board/domain/auth/dto/AuthenticationResponse.java
@@ -1,6 +1,9 @@
 package com.snowball.board.domain.auth.dto;
 
-
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +13,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AuthenticationResponse {
 
-    private String token;
+    private String accessToken;
+    private String refreshToken;
 }
