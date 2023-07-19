06a6d0647a786fd3c0ba2810605d7a5a02b4f618
JY-B <jongyoonb+u@gmail.com>
2023-07-19 12:50:30 +0900

    feat: #11 - add UpdateInfoResponse

--- src/main/java/com/snowball/board/domain/user/dto/UpdateInfoResponse.java
+++ src/main/java/com/snowball/board/domain/user/dto/UpdateInfoResponse.java
@@ -1,5 +1,6 @@
 package com.snowball.board.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
@@ -7,6 +8,7 @@ import org.hibernate.validator.constraints.Length;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UpdateInfoResponse {
 
     @Length(max = 30)
