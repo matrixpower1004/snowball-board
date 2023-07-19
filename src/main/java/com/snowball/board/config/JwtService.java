b9a0d648842c4f06f233be5413dfc3de90c148bd
JY-B <jongyoonb+u@gmail.com>
2023-07-19 12:53:04 +0900

    feat: #11 - modify JwtService
    add generateRefreshToken
    modify method to call buildToken method to generate token

--- src/main/java/com/snowball/board/config/JwtService.java
+++ src/main/java/com/snowball/board/config/JwtService.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +19,14 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "0YLkkTt8BMSJIwLyR6GSgaCgDcbE5tbzawnTNT6d2EUEECeWm0KpLYVxXKTezwPL";
+    @Value("${application.security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    @Value("${application.security.jwt.expiration}")
+    private Long EXPIRATION;
+
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private Long REFRESH_EXPIRATION;
 
     public String extractUserAccount(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -29,17 +38,15 @@ public class JwtService {
     }
 
     public String generateToken(Map<String, Object>extraClaims, UserDetails userDetails) {
-        return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
+        return buildToken(extraClaims, userDetails, EXPIRATION);
     }
 
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        return buildToken(new HashMap<>(), userDetails, REFRESH_EXPIRATION);
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
@@ -67,5 +74,28 @@ public class JwtService {
     private Key getSignInKey() {
         byte[] keyBytes =  Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration)
+    {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public ResponseCookie generateCookie(String refreshToken) {
+        return ResponseCookie.from("refresh_token", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/")
+                .build();
     }
 }
