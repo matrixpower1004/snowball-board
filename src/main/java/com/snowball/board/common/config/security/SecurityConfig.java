83c90122ca7ab5dcd6de3eb6511d5ce7ffda9ca2
Coding9nt <qkralsrl1226@gmail.com>
2023-07-19 13:15:32 +0900

    feat:BoardControler,Spring SecurityConfig Add

---
+++ src/main/java/com/snowball/board/common/config/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.snowball.board.common.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                        .antMatchers("/h2-console/**").permitAll() // H2 콘솔 접근 허용
+                        .antMatchers("/**/*", "/home").permitAll()
+                        .antMatchers("/board").permitAll() // /board 경로에 대한 인증 필요 없음
+                        .anyRequest().authenticated()
+                        .and()
+                .formLogin()
+                        .loginPage("/login")
+                        .permitAll()
+                        .and()
+                .logout()
+                        .permitAll();
+
+        // H2 콘솔 사용을 위한 추가 설정
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}password")
+                .roles("USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/board"); // 해당 경로를 Spring Security의 필터 체인에서 제외
+    }
+}
