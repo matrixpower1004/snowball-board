528b6041ebabe4ac88a85b0a9a7377985408db23
JY-B <jongyoonb+u@gmail.com>
2023-07-20 15:09:43 +0900

    feat: #11 - add UserController
    add controller for user view

---
+++ src/main/java/com/snowball/board/domain/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.snowball.board.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+}
