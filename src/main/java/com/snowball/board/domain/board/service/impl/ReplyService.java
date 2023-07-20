b4aa62c79fc6f9bbc90f7bce51d48bbfe09aebd7
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-07-20 21:07:31 +0900

    reply 수정

--- src/main/java/com/snowball/board/domain/board/service/impl/ReplyService.java
+++ src/main/java/com/snowball/board/domain/board/service/impl/ReplyService.java
@@ -1,11 +1,13 @@
-package com.snowball.board.domain.board.service;
+package com.snowball.board.domain.board.service.impl;
 
-import com.snowball.board.domain.board.dto.ReplyDTO;
-import com.snowball.board.domain.board.emtity.Comment;
-import com.snowball.board.domain.board.emtity.Reply;
-import com.snowball.board.domain.board.emtity.User;
+import com.snowball.board.domain.board.dto.ReplyDto;
+import com.snowball.board.domain.board.model.Comment;
+import com.snowball.board.domain.board.model.Reply;
 import com.snowball.board.domain.board.repository.CommentRepository;
 import com.snowball.board.domain.board.repository.ReplyRepository;
+import com.snowball.board.domain.user.model.User;
+import com.snowball.board.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,18 +16,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ReplyService {
-
     private final ReplyRepository replyRepository;
     private final CommentRepository commentRepository;
-
-    public ReplyService(ReplyRepository replyRepository, CommentRepository commentRepository) {
-        this.replyRepository = replyRepository;
-        this.commentRepository = commentRepository;
-    }
+    private final UserRepository userRepository;
 
     @Transactional
-    public ReplyDTO createReply(Long commentId, String content, Long userId) {
+    public ReplyDto createReply(Long commentId, String content, Long userId) {
         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 
         Comment comment = commentRepository.findById(commentId).orElse(null);
@@ -33,22 +31,25 @@ public class ReplyService {
             return null;
         }
 
-        User user = new User();
-        user.setId(userId);
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            return null;
+        }
 
-        Reply reply = new Reply();
-        reply.setComment(comment);
-        reply.setUser(user);
-        reply.setContent(content);
-        reply.setCreatedAt(currentTime);
-        reply.setUpdatedAt(currentTime);
+        Reply reply = Reply.builder()
+                .comment(comment)
+                .user(user)
+                .content(content)
+                .createdAt(currentTime)
+                .updatedAt(currentTime)
+                .build();
 
         Reply savedReply = replyRepository.save(reply);
         return mapToDTO(savedReply);
     }
 
     @Transactional
-    public ReplyDTO updateReply(Long replyId, String content) {
+    public ReplyDto updateReply(Long replyId, String content) {
         Reply reply = replyRepository.findById(replyId).orElse(null);
         if (reply != null) {
             reply.setContent(content);
@@ -58,19 +59,18 @@ public class ReplyService {
         }
         return null;
     }
-
     @Transactional
     public void deleteReply(Long replyId) {
         replyRepository.deleteById(replyId);
     }
 
-    public List<ReplyDTO> getAllRepliesByCommentId(Long commentId) {
+    public List<ReplyDto> getAllRepliesByCommentId(Long commentId) {
         List<Reply> replies = replyRepository.findAllByCommentId(commentId);
         return replies.stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    private ReplyDTO mapToDTO(Reply reply) {
-        ReplyDTO replyDTO = new ReplyDTO();
+    private ReplyDto mapToDTO(Reply reply) {
+        ReplyDto replyDTO = new ReplyDto();
         replyDTO.setId(reply.getId());
         replyDTO.setUserId(reply.getUser().getId());
         replyDTO.setCommentId(reply.getComment().getId());
