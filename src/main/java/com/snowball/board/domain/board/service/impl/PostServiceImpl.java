e497fb4cefeeee8cb204ba237b61be15a1785046
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-07-21 02:15:53 +0900

    post 수정

--- src/main/java/com/snowball/board/domain/board/service/impl/PostServiceImpl.java
+++ src/main/java/com/snowball/board/domain/board/service/impl/PostServiceImpl.java
@@ -1,11 +1,12 @@
 package com.snowball.board.domain.board.service.impl;
 
+import com.snowball.board.common.exception.model.NotFoundException;
 import com.snowball.board.domain.board.model.Post;
+import com.snowball.board.domain.board.repository.CommentRepository;
 import com.snowball.board.domain.board.repository.PostRepository;
 import com.snowball.board.domain.board.dto.PostDto;
 import com.snowball.board.domain.board.mapper.PostsMapper;
 import com.snowball.board.domain.user.repository.UserRepository;
-import com.snowball.board.common.exception.model.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +15,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +24,8 @@ public class PostServiceImpl implements com.snowball.board.domain.board.service.
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final PostsMapper postsMapper;
+    private final CommentRepository commentRepository;
+
     @Transactional
     @Override
     public Page<PostDto> getPostsByPage(int page, int size) {
@@ -49,8 +54,12 @@ public class PostServiceImpl implements com.snowball.board.domain.board.service.
     public PostDto updatePost(Long postId, PostDto postDto) {
         Post existingPost = postRepository.findById(postId)
                 .orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다. ID: " + postId));
-        existingPost.setTitle(postDto.getTitle());
-        existingPost.setContent(postDto.getContent());
+        if (postDto.getTitle() != null) {
+            existingPost.setTitle(postDto.getTitle());
+        }
+        if (postDto.getContent() != null) {
+            existingPost.setContent(postDto.getContent());
+        }
         existingPost.setBlindState(postDto.isBlindState());
         Post updatedPost = postRepository.save(existingPost);
         return postsMapper.mapToDto(updatedPost);
@@ -62,6 +71,13 @@ public class PostServiceImpl implements com.snowball.board.domain.board.service.
         if (!postRepository.existsById(postId)) {
             throw new NotFoundException("게시물을 찾을 수 없습니다. ID: " + postId);
         }
+        commentRepository.deleteByPostId(postId);
         postRepository.deleteById(postId);
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(postsMapper::mapToDto).collect(Collectors.toList());
     }
 }
