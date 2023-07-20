b301a0e06527a213bc23660af70404345b5847a7
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-07-20 21:07:03 +0900

    user 수정

--- src/main/java/com/snowball/board/common/util/UserRole.java
+++ src/main/java/com/snowball/board/common/util/UserRole.java
@@ -4,8 +4,7 @@ public enum UserRole {
     BLACK("블랙리스트"),
     BEGINNER("새싹회원"),
     EXPERT("우수회원"),
-    ADMIN("관리자"),
-    USER("일반회원"); // USER 상수 추가
+    ADMIN("관리자");
 
     private final String value;
 
