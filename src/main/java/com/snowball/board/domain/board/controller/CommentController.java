92a812da40feb15b9d4e89d31b447789d23ed860
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-07-20 21:08:02 +0900

    comment 수정

--- src/main/java/com/snowball/board/domain/board/controller/CommentController.java
+++ src/main/java/com/snowball/board/domain/board/controller/CommentController.java
@@ -1,55 +1,49 @@
 package com.snowball.board.domain.board.controller;
 
-import com.snowball.board.domain.board.dto.CommentDTO;
-import com.snowball.board.domain.board.service.CommentService;
-import com.snowball.board.domain.board.service.ReplyService;
+import com.snowball.board.domain.board.dto.CommentDto;
+import com.snowball.board.domain.board.service.impl.CommentService;
+import com.snowball.board.domain.board.service.impl.ReplyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
     private final ReplyService replyService;
-
-    public CommentController(CommentService commentService, ReplyService replyService) {
-        this.commentService = commentService;
-        this.replyService = replyService;
-    }
-
-    @PostMapping("/post/{postId}/comment")
-    public ResponseEntity<CommentDTO> createComment(@PathVariable Long postId, @RequestBody String content) {
-        CommentDTO createdComment = commentService.createComment(postId, content);
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable Long postId, @RequestBody String content) {
+        CommentDto createdComment = commentService.createComment(postId, content);
         if (createdComment != null) {
             return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/comment/{commentId}")
-    public ResponseEntity<CommentDTO> updateComment(@PathVariable Long commentId, @RequestBody String content) {
-        CommentDTO updatedComment = commentService.updateComment(commentId, content);
+    @PutMapping("/comments/{commentId}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable Long commentId, @RequestBody String content) {
+        CommentDto updatedComment = commentService.updateComment(commentId, content);
         if (updatedComment != null) {
             return new ResponseEntity<>(updatedComment, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/comment/{commentId}")
+    @DeleteMapping("/comments/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
         commentService.deleteComment(commentId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping("/post/{postId}/comments")
-    public ResponseEntity<List<CommentDTO>> getAllCommentsByPostId(@PathVariable Long postId) {
-        List<CommentDTO> comments = commentService.getAllCommentsByPostId(postId);
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<List<CommentDto>> getAllCommentsByPostId(@PathVariable Long postId) {
+        List<CommentDto> comments = commentService.getAllCommentsByPostId(postId);
         return new ResponseEntity<>(comments, HttpStatus.OK);
     }
-
 
 }
