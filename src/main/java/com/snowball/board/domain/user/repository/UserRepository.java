4b3eba42a9f560f0687adc78a841b8754317900e
JY-B <jongyoonb+u@gmail.com>
2023-07-22 18:00:51 +0900

    refactor: #28 - user Repository
    add annotation
    add method for batch job
    add email check

--- src/main/java/com/snowball/board/domain/user/repository/UserRepository.java
+++ src/main/java/com/snowball/board/domain/user/repository/UserRepository.java
@@ -5,15 +5,20 @@ import com.snowball.board.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserAccount(String userAccount);
 
     Optional<User> findByNickName(String nickName);
+
+    Optional<User> findByEmail(String email);
+
 
     @Query("SELECT u, COUNT(p.id) FROM User u LEFT JOIN Post p ON u.id = p.user.id GROUP BY u.id HAVING COUNT(p.id) > :postCountThreshold")
     List<User> getUsersWithPostCountGreaterThan(@Param("postCountThreshold") Long postCountThreshold);
