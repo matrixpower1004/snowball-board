e700f648fe45d7c9126e479ce081d812278f1a89
JY-B <jongyoonb+u@gmail.com>
2023-07-22 18:09:39 +0900

    refactor: #28 - Add WebConfig
    add methods for config

---
+++ src/main/java/com/snowball/board/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.snowball.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/", "classpath:/public/",
+            "classpath:/", "classpath:/resources/", "classpath:/META-INF/resources/",
+            "classpath:/META-INF/resources/webjars/"};
+
+    // root will redirect to /login
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    // mapping static resources
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    // password Encoder
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
