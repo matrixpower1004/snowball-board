b1fee4cea9b393fb7bd55353808f8e67f38e46db
JY-B <jongyoonb+u@gmail.com>
2023-07-22 18:05:33 +0900

    refactor: #28 - modify Board Controller
    modify getMapping for test

--- src/main/java/com/snowball/board/web/controller/BoardController.java
+++ src/main/java/com/snowball/board/web/controller/BoardController.java
@@ -1,22 +1,16 @@
 package com.snowball.board.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+//Test Controller for board
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
     @GetMapping
     public String showBoardList() {
-        return "board_list";
+        return "board";
     }
 
-    @GetMapping("/details")
-    public String showBoardDetails() {
-        return "board_details";
-    }
 }
